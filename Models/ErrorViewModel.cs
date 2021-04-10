b521c35d15b88e1ee03e726b6ce5ee97f0a58093
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2021-04-10 12:04:03 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RSVP_Party_Invite_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
